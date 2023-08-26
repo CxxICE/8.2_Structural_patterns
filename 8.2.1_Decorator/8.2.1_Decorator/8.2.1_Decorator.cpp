c4754d59b4831d042f54076f016512ce6c300f89
Ilia Surikov <surikovis89@gmail.com>
2023-08-26 18:26:10 +0300

    Initial commit

---
+++ 8.2.1_Decorator/8.2.1_Decorator/8.2.1_Decorator.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Text 
+{
+public:
+	virtual void render(const std::string &data, const std::string &href = "") const
+	{
+		std::cout << data;
+	}
+};
+
+
+class DecoratedText : public Text 
+{
+public:
+	explicit DecoratedText(Text *text) : text_(text) {}
+	Text *text_;
+};
+
+class ItalicText : public DecoratedText 
+{
+public:
+	explicit ItalicText(Text *text) : DecoratedText(text) {}
+	void render(const std::string &data, const std::string &href = "") const override
+	{
+		std::cout << "<i>";
+		text_->render(data, href);
+		std::cout << "</i>";
+	}
+};
+
+class BoldText : public DecoratedText 
+{
+public:
+	explicit BoldText(Text *text) : DecoratedText(text) {}
+	void render(const std::string &data, const std::string &href = "") const override
+	{
+		std::cout << "<b>";
+		text_->render(data, href);
+		std::cout << "</b>";
+	}
+};
+
+class Paragraph : public DecoratedText 
+{
+public:
+	explicit Paragraph(Text *text) : DecoratedText(text) {}
+	void render(const std::string &data, const std::string &href = "") const override
+	{
+		std::cout << "<p>";
+		text_->render(data, href);
+		std::cout << "</p>";
+	}
+};
+
+class Reversed : public DecoratedText 
+{
+public:
+	explicit Reversed(Text *text) : DecoratedText(text) {}
+	void render(const std::string &data, const std::string &href = "") const override
+	{
+		std::string tmp = data;
+		//auto it_r = tmp.rbegin();
+		//for (auto it_f = tmp.begin(); it_f < it_r.base(); ++it_f, ++it_r)
+		//{
+		//	std::swap(*it_f, *it_r);
+		//}
+		std::reverse(tmp.begin(), tmp.end());
+		text_->render(tmp, href);
+	}
+	
+};
+
+class Link : public DecoratedText 
+{
+public:
+	explicit Link(Text *text) : DecoratedText(text) {}
+	void render(const std::string &data, const std::string &href) const override
+	{
+		std::cout << "<a href=" << href << ">";
+		text_->render(data, href);
+		std::cout << "</a>";
+	}
+};
+
+int main() 
+{
+	{
+		std::cout << "\x1B[36m" << "ItalicText + BoldText" << "\x1B[0m" << std::endl;
+		auto text_block = new ItalicText(new BoldText(new Text()));
+		text_block->render("Hello world");
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "\x1B[36m" << "Paragraph" << "\x1B[0m" << std::endl;
+		auto text_block = new Paragraph(new Text());
+		text_block->render("Hello world");
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "\x1B[36m" << "Link" << "\x1B[0m" << std::endl;
+		auto text_block = new Link(new Text());
+		text_block->render("Hello world", "netology.ru");
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "\x1B[36m" << "ItalicText + Link" << "\x1B[0m" << std::endl;
+		auto text_block = new ItalicText(new Link(new Text()));
+		text_block->render("Hello world", "netology.ru");
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "\x1B[36m" << "Reversed" << "\x1B[0m" << std::endl;
+		auto text_block = new Reversed(new Text());
+		text_block->render("Hello world");
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "\x1B[36m" << "BoldText + Reversed + ItalicText" << "\x1B[0m" << std::endl;
+		auto text_block = new BoldText(new Reversed(new ItalicText(new Text())));
+		text_block->render("Hello world");
+		std::cout << std::endl;
+	}
+	
+	{
+		std::cout << "\x1B[36m" << "BoldText + Reversed + ItalicText + Link + Paragraph" << "\x1B[0m" << std::endl;
+		auto text_block = new BoldText(new Reversed(new ItalicText(new Link(new Paragraph(new Text())))));
+		text_block->render("Hello world", "netology.ru");
+		std::cout << std::endl;
+	}
+}
---
+++ 8.2.2_Proxy/8.2.2_Proxy/8.2.2_Proxy.cpp
@@ -0,0 +1,91 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase 
+{
+public:
+	std::string GetData(const std::string &key) const noexcept 
+	{
+		return "Very Big Data String: " + key;
+	}
+};
+
+class CacheProxyDB : VeryHeavyDatabase 
+{
+public:
+	explicit CacheProxyDB(VeryHeavyDatabase *real_object) : real_db_(real_object) {}
+	std::string GetData(const std::string &key) noexcept 
+	{
+		if (cache_.find(key) == cache_.end()) 
+		{
+			std::cout << "Get from real object\n";
+			cache_[key] = real_db_->GetData(key);
+		}
+		else 
+		{
+			std::cout << "Get from cache\n";
+		}
+		return cache_.at(key);
+	}
+
+private:
+	std::map<std::string, std::string> cache_;
+	VeryHeavyDatabase *real_db_;
+};
+
+class TestDB : VeryHeavyDatabase 
+{
+public:
+	explicit TestDB(VeryHeavyDatabase *real_object) : real_db_(real_object) {}
+	std::string GetData(const std::string &key) const noexcept 
+	{
+		return "test_data\n";
+	}
+private:
+	VeryHeavyDatabase *real_db_;
+};
+
+
+class OneShotDB : VeryHeavyDatabase
+{
+public:
+	explicit OneShotDB(VeryHeavyDatabase *real_object, size_t shots = 1) : real_db_(real_object), _shots(shots) {};
+	std::string GetData(const std::string &key) noexcept
+	{
+		if (_shots > 0)
+		{
+			--_shots;
+			return real_db_->GetData(key);
+		}
+		else
+		{
+			return "error";
+		}		
+	}
+private:
+	VeryHeavyDatabase *real_db_;
+	size_t _shots;
+};
+
+int main() 
+{
+	/*
+	{
+		auto real_db = VeryHeavyDatabase();
+		auto cached_db = CacheProxyDB(std::addressof(real_db));
+		auto test_db = TestDB(std::addressof(real_db));
+		std::cout << cached_db.GetData("key") << std::endl;
+		std::cout << cached_db.GetData("key") << std::endl;
+		std::cout << test_db.GetData("key") << std::endl;
+	}
+	*/
+	{
+		auto real_db = VeryHeavyDatabase();
+		auto limit_db = OneShotDB(std::addressof(real_db), 2);
+		std::cout << limit_db.GetData("key") << std::endl;
+		std::cout << limit_db.GetData("key") << std::endl;
+		std::cout << limit_db.GetData("key") << std::endl;
+	}
+	return 0;
+}
